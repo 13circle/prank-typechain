0a40ea8628655e4b281af68830fb045effae3553
13circle <13circle97@gmail.com>
2021-01-04 00:19:14 +0900

    Lec 0.7 - Classes on Typescript part One
    * Explicitly show properties in a class in js script
      - TS interface does not show properties explicitly in js script
      - To show properties explicitly in js code, class is used often
    
    * Add Typescript class
      - Set permission of the properties
      - Set types of the properties & parameters in the constructor

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "Yee",
-  gender: "male",
-  age: 23,
-};
+const person = new Human("Yee", 23, "male");
 
 const func = (person: Human): string => {
   return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
