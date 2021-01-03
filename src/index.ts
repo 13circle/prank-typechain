8acc11ae02be07d76407fd9583f9ddbb4571325e
13circle <13circle97@gmail.com>
2021-01-04 01:14:52 +0900

    Lec 0.8 - Blockchain Creating a Block
    * Create Block class
      - Same as previous lecture
    * Declare Array type
      - [TYPE_NAME] means array type of TYPE_NAME

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,28 @@
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
-  constructor(name: string, age: number, gender: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
 
-const person = new Human("Yee", 23, "male");
+const genesisBlock: Block = new Block(0, "a23hj0nioawseh09a349", "", "YEE", 123456);
 
-const func = (person: Human): string => {
-  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
-};
+let blockchain: [Block] = [genesisBlock];
 
-console.log(func(person));
+console.log(blockchain);
 
 export {};
