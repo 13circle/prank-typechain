05e3eb10b157ba6e80bb2f166b225d174b40ce01
13circle <13circle97@gmail.com>
2021-01-03 12:50:36 +0900

    Lec 0.5 - First steps with Typescript
    * Typescript is a typed javascript
      - Will be explained detail at next lecture
    * Typescript strictly handles syntax for function parameters
      - Error unless a number of given parameters does not match
      - Can make a parameter as optional by adding '?' as the affix

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("Hello, TypeScript!");
+const name = "Yee",
+  age = 23,
+  gender = "male";
+
+const func = (name, age, gender?) => {
+  console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+func(name, age);
+
+export {};
