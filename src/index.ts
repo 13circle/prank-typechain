4196f0d6a20734996287b82cb0a7889118f07a86
13circle <13circle97@gmail.com>
2021-01-03 13:13:12 +0900

    Lec 0.5 - Types in Typescript
    * Set a specific type for a variable
      - Sets types for function parameters & return type in this lecture
    * Configure typescript
      - Install tsc-watch (roughly equivalent to nodemon in js)
      - Add dev command to setup watch mode
      - Handle all source codes for this project in src directory for now on
      - Include all source codes in src directory into tsconfig.json

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const func = (name: string, age: number, gender: string): string => {
+  return `Hello ${name}, you are ${age}, you are a ${gender}`;
+};
+
+console.log(func("Yee", 23, "male"));
+
+export {};
