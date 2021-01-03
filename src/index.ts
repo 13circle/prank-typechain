2ba5f9ef5c4112d2a4d1672b951a5aa43b7a1cd6
13circle <13circle97@gmail.com>
2021-01-04 01:31:42 +0900

    Lec 0.9 - Creating a Block part Two
    * Install crypto-js
      - To generate a hash string
    * Add static method to Block class
      - calculateBlockHash
    * Add functions to use blockchain efficiently
      - getBlockchain: returns an array of Blocks, aka blockchain
      - getLatestBlock: returns the last Block in the blockchain
      - getNewTimestamp: returns a timestamp of a Block in seconds

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
   constructor(
     index: number,
     hash: string,
@@ -19,10 +30,20 @@ class Block {
   }
 }
 
-const genesisBlock: Block = new Block(0, "a23hj0nioawseh09a349", "", "YEE", 123456);
+const genesisBlock: Block = new Block(
+  0,
+  "a23hj0nioawseh09a349",
+  "",
+  "YEE",
+  123456
+);
 
 let blockchain: [Block] = [genesisBlock];
 
-console.log(blockchain);
+const getBlockchain = (): [Block] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimestamp = (): number => Math.round(new Date().getTime() / 1000);
 
 export {};
