b6275235b941ae278a6436933b15207f544950f8
13circle <13circle97@gmail.com>
2021-01-05 18:54:04 +0900

    Lec 0.13 - Conclusion
    * Push new block to the blockchain at creation
      - addBlock(newBlock)
    * Call createNewBlock for test
      - Check if indices are updated well
      - Check if the previousHash is equal to hash of the previous block

--- src/index.ts
+++ src/index.ts
@@ -75,6 +75,7 @@ const createNewBlock = (data: string): Block => {
     data,
     newTimestamp
   );
+  addBlock(newBlock);
   return newBlock;
 };
 
@@ -104,5 +105,11 @@ const addBlock = (candidateBlock: Block): void => {
     blockchain.push(candidateBlock);
   }
 };
+
+createNewBlock("second block");
+createNewBlock("third block");
+createNewBlock("fourth block");
+
+console.log(blockchain);
 
 export {};
